92e252f0c8d28110600ce25634a75fb47f6fd72d
Vaspera <110002885+Vazpera@users.noreply.github.com>
2024-09-25 18:22:34 +0000

    Beginning

---
+++ src/cards.rs
@@ -0,0 +1,87 @@
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Debug, EnumIter, Clone, Copy)]
+pub enum Suit {
+    Spade,
+    Heart,
+    Club,
+    Diamond,
+}
+
+impl std::fmt::Display for Suit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Suit::Club => "♣",
+                Suit::Spade => "♠",
+                Suit::Diamond => "♦",
+                Suit::Heart => "♥",
+            }
+        )
+    }
+}
+#[derive(Debug, EnumIter, Clone, Copy)]
+pub enum Rank {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl std::fmt::Display for Rank {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Rank::Ace => "A",
+                Rank::King => "K",
+                Rank::Queen => "Q",
+                Rank::Jack => "J",
+                Rank::Two => "2",
+                Rank::Three => "3",
+                Rank::Four => "4",
+                Rank::Five => "5",
+                Rank::Six => "6",
+                Rank::Seven => "7",
+                Rank::Eight => "8",
+                Rank::Nine => "9",
+                Rank::Ten => "10",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+impl std::fmt::Display for Card {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.suit, self.rank)
+    }
+}
+
+pub fn new_deck() -> Vec<Card> {
+    let mut cards: Vec<Card> = Vec::new();
+    for suit in Suit::iter() {
+        for rank in Rank::iter() {
+            cards.push(Card { suit, rank })
+        }
+    }
+    cards
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-pub mod func;
+pub mod cards;
+use std::ops::Deref;
+
+use cards::{Card, Rank, Suit};
 
 fn main() {
-    println!(
-        "Default {}",
-        func::Value::default().set_value(2).get_value()
-    );
+    let deck = cards::new_deck();
+    let lines = deck
+        .into_iter()
+        .enumerate()
+        .fold(vec!["".to_string()], |acc, (i, x)| {
+            let curr = acc.clone().last().unwrap().to_owned();
+            let mut next = acc.clone();
+            if i % 13 == 0 {
+                next.push(format!("{}", x));
+            } else {
+                next = next
+                    .clone()
+                    .into_iter()
+                    .take(acc.clone().len() - 1)
+                    .collect::<Vec<String>>();
+                next.push(curr + &format!(" {}", x).as_str());
+            }
+            next
+        });
+    for line in lines {
+        println!("{line}")
+    }
 }
+
